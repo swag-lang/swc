0512ac6e8643ab4bf7281cb3630015ecd7bef913
cb <christophe.beaudet@gmail.com>
2025-10-24 10:41:44 +0200

    Add `makeNode` method to `Ast` for creating and storing AST nodes
    - Implement `makeNode` method in `Ast` to simplify AST node creation.
    - Update `Ast.h` to declare the new method with default values for `left` and `right`.

--- src/Parser/Ast.h
+++ src/Parser/Ast.h
@@ -8,6 +8,9 @@ class Ast
 {
     std::vector<AstNode> nodes_;
     AstExtStore          extensions_;
+
+public:
+    AstNodeRef makeNode(AstNodeId id, FileRef file, TokenRef token, AstNodeRef left = INVALID_AST_NODE_REF, uint32_t right = INVALID_AST_NODE_REF);
 };
 
 SWC_END_NAMESPACE();

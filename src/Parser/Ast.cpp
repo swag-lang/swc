3012317a5135164a5eefaae8f7a4705fa794d970
cb <christophe.beaudet@gmail.com>
2025-10-24 10:47:08 +0200

    Refactor: Inline `makeNode` method in `Ast` and add `node` accessor
    - Move `makeNode` method implementation from `Ast.cpp` to header for better readability and coherence.
    - Introduce `node` method in `Ast` for direct access to AST nodes.

--- src/Parser/Ast.cpp
+++ src/Parser/Ast.cpp
@@ -1,19 +1,6 @@
 #include "pch.h"
 #include "Parser/Ast.h"
-#include "Core/Types.h"
 
 SWC_BEGIN_NAMESPACE();
-
-AstNodeRef Ast::makeNode(AstNodeId id, FileRef file, TokenRef token, AstNodeRef left, AstNodeRef right)
-{
-    AstNode n;
-    n.id    = id;
-    n.file  = file;
-    n.token = token;
-    n.left  = left;
-    n.right = right;
-    nodes_.push_back(n);
-    return static_cast<AstNodeRef>(nodes_.size()) - 1;
-}
 
 SWC_END_NAMESPACE();
--- src/Parser/Ast.h
+++ src/Parser/Ast.h
@@ -10,7 +10,13 @@ class Ast
     AstExtStore          extensions_;
 
 public:
-    AstNodeRef makeNode(AstNodeId id, FileRef file, TokenRef token, AstNodeRef left = INVALID_AST_NODE_REF, uint32_t right = INVALID_AST_NODE_REF);
+    AstNodeRef makeNode(AstNodeId id, FileRef file, TokenRef token, AstNodeRef left = INVALID_AST_NODE_REF, uint32_t right = INVALID_AST_NODE_REF)
+    {
+        nodes_.emplace_back(id, AstNodeFlagsEnum::Zero, file, token, left, right);
+        return static_cast<AstNodeRef>(nodes_.size() - 1);
+    }
+
+    AstNode* node(AstNodeRef ref) { return &nodes_[ref]; }
 };
 
 SWC_END_NAMESPACE();

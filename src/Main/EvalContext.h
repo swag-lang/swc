ac16e7311d14211e748705b2a428171da7a48489
cb <christophe.beaudet@gmail.com>
2025-10-24 08:42:00 +0200

    Refactor: Update `EvalContext` to use pointer for `CompilerContext` and improve null safety
    - Replace `CompilerContext` reference with pointer in `EvalContext` for better null safety.
    - Adjust member initialization and method implementations accordingly.

--- src/Main/EvalContext.h
+++ src/Main/EvalContext.h
@@ -3,21 +3,24 @@
 
 SWC_BEGIN_NAMESPACE()
 
+class CompilerContext;
+class Global;
+struct CommandLine;
 class SourceFile;
 
 class EvalContext
 {
-    const CompilerContext& cmpContext;
+    const CompilerContext* cmpContext_ = nullptr;
     SourceFile*            sourceFile_ = nullptr;
 
 public:
     explicit EvalContext(const CompilerContext& compContext) :
-        cmpContext(compContext)
+        cmpContext_(&compContext)
     {
     }
 
-    const Global&      global() const { return cmpContext.global(); }
-    const CommandLine& cmdLine() const { return cmpContext.cmdLine(); }
+    const Global&      global() const { return cmpContext_->global(); }
+    const CommandLine& cmdLine() const { return cmpContext_->cmdLine(); }
     SourceFile*        sourceFile() const { return sourceFile_; }
     void               setSourceFile(SourceFile* sourceFile) { sourceFile_ = sourceFile; }
 };

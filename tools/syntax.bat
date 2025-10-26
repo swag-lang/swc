c8813afaa7e280028c3212450955d46a159c4404
cb <christophe.beaudet@gmail.com>
2025-10-26 09:19:45 +0100

    Add `@echo off` to `syntax.bat` to suppress command echoing

--- tools/syntax.bat
+++ tools/syntax.bat
@@ -1,7 +1,8 @@
+@echo off
+
 swc syntax --folder ../tests %1 %2 %3 %4 %5
 swc syntax --folder ../../swag/bin/testsuite/tests/compiler/src/legacy %1 %2 %3 %4 %5
 swc syntax --folder ../../swag/bin/std %1 %2 %3 %4 %5
 swc syntax --folder ../../swag/bin/examples %1 %2 %3 %4 %5
 swc syntax --folder ../../swag/bin/runtime %1 %2 %3 %4 %5
 swc syntax --folder ../../swag/bin/reference %1 %2 %3 %4 %5
-

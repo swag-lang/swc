3b698b30f48a9e633510182ece0234da95f342a0
cb <christophe.beaudet@gmail.com>
2025-11-02 17:01:34 +0100

    Refactor and relocate literal parsing logic to `Parser_Literal_`
    - Move `parseLiteral`, `parseLiteralExpression`, and `parseLiteralArray` from `Parser_Expression_` to the new `Parser_Literal_` file for better modularity.
    - Update project files to include `Parser_Literal_`.
    - Streamline `Parser_Expression_` by removing the relocated methods.

--- src/Parser/Parser_Expression_.cpp
+++ src/Parser/Parser_Expression_.cpp
@@ -4,115 +4,6 @@
 
 SWC_BEGIN_NAMESPACE()
 
-AstNodeRef Parser::parseLiteral()
-{
-    std::pair<AstNodeRef, AstNodeLiteral*> literal;
-    switch (id())
-    {
-    case TokenId::NumberInteger:
-    case TokenId::NumberBinary:
-    case TokenId::NumberHexadecimal:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::IntegerLiteral);
-        break;
-
-    case TokenId::NumberFloat:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::FloatLiteral);
-        break;
-
-    case TokenId::StringLine:
-    case TokenId::StringRaw:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::StringLiteral);
-        break;
-
-    case TokenId::Character:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::CharacterLiteral);
-        break;
-
-    case TokenId::KwdTrue:
-    case TokenId::KwdFalse:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::BoolLiteral);
-        break;
-
-    case TokenId::KwdNull:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::NullLiteral);
-        break;
-
-    case TokenId::CompilerFile:
-    case TokenId::CompilerModule:
-    case TokenId::CompilerLine:
-    case TokenId::CompilerBuildVersion:
-    case TokenId::CompilerBuildRevision:
-    case TokenId::CompilerBuildNum:
-    case TokenId::CompilerBuildCfg:
-    case TokenId::CompilerCallerFunction:
-    case TokenId::CompilerCallerLocation:
-    case TokenId::CompilerOs:
-    case TokenId::CompilerArch:
-    case TokenId::CompilerCpu:
-    case TokenId::CompilerSwagOs:
-    case TokenId::CompilerBackend:
-        literal = ast_->makeNode<AstNodeLiteral>(AstNodeId::CompilerLiteral);
-        break;
-
-    default:
-        (void) reportError(DiagnosticId::ParserExpectedTokenFam, tok());
-        return INVALID_REF;
-    }
-
-    literal.second->tknValue = consume();
-    return literal.first;
-}
-
-AstNodeRef Parser::parseLiteralExpression()
-{
-    const auto literal = parseLiteral();
-    if (isInvalid(literal))
-        return INVALID_REF;
-
-    const auto quoteTknRef = ref();
-    if (!consumeIf(TokenId::SymQuote))
-        return literal;
-
-    const auto [nodeRef, nodePtr] = ast_->makeNode<AstNodeId::QuotedLiteral>();
-    nodePtr->nodeLiteral          = literal;
-
-    switch (id())
-    {
-    case TokenId::Identifier:
-        nodePtr->nodeQuote = parseIdentifier();
-        return nodeRef;
-
-    case TokenId::TypeF32:
-    case TokenId::TypeF64:
-    case TokenId::TypeS8:
-    case TokenId::TypeS16:
-    case TokenId::TypeS32:
-    case TokenId::TypeS64:
-    case TokenId::TypeU8:
-    case TokenId::TypeU16:
-    case TokenId::TypeU32:
-    case TokenId::TypeU64:
-    case TokenId::TypeRune:
-    case TokenId::TypeBool:
-        nodePtr->nodeQuote = parseType();
-        return nodeRef;
-
-    case TokenId::TypeAny:
-    case TokenId::TypeCString:
-    case TokenId::TypeCVarArgs:
-    case TokenId::TypeString:
-    case TokenId::TypeTypeInfo:
-    case TokenId::TypeVoid:
-        (void) reportError(DiagnosticId::ParserInvalidLiteralSuffix, tok());
-        consume();
-        return nodeRef;
-
-    default:
-        (void) reportError(DiagnosticId::ParserEmptyLiteralSuffix, quoteTknRef);
-        return nodeRef;
-    }
-}
-
 AstNodeRef Parser::parsePrimaryExpression()
 {
     switch (id())
@@ -293,11 +184,6 @@ AstNodeRef Parser::parseIdentifierExpression()
     }
 
     return parseIdentifier();
-}
-
-AstNodeRef Parser::parseLiteralArray()
-{
-    return parseBlock(AstNodeId::ArrayLiteral, TokenId::SymLeftBracket);
 }
 
 AstNodeRef Parser::parseNamedArgument()

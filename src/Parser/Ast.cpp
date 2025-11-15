b0c291048057a16e5f09098929d3f33c4f4852c9
cb <christophe.beaudet@gmail.com>
2025-11-15 08:53:34 +0100

    Refactor: Append AST node chunks directly to output in `Ast::nodes`

--- src/Parser/Ast.cpp
+++ src/Parser/Ast.cpp
@@ -11,8 +11,7 @@ void Ast::nodes(SmallVector<AstNodeRef>& out, SpanRef spanRef) const
 
     const RefStore<>::SpanView<AstNodeRef> view{&store_, spanRef.get()};
     for (auto it = view.chunks_begin(); it != view.chunks_end(); ++it)
-    {
-    }
+        out.append(it.current.ptr, it.current.count);
 }
 
 SWC_END_NAMESPACE()

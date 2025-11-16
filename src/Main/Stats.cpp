68cdb3a3f6602f3bfb0a0bbd29a0e03876eaadc1
cb <christophe.beaudet@gmail.com>
2025-11-16 11:29:05 +0100

    Refactor: Add batch format script and improve `Stats` logging structure
    - Added `tools/format.bat` script for batch code formatting using SWC.
    - Enhanced `Stats` logging by restructuring output blocks and adding `timeTotal` outside conditional stats.

--- src/Main/Stats.cpp
+++ src/Main/Stats.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
+#include "Main/Stats.h"
 #include "Core/Timer.h"
 #include "Core/Utf8Helper.h"
 #include "Main/Global.h"
-#include "Main/Stats.h"
 #include "Report/LogColor.h"
 #include "Report/Logger.h"
 #include "Thread/JobManager.h"
@@ -17,31 +17,28 @@ void Stats::print(const TaskContext& ctx) const
     constexpr auto colorHeader = LogColor::Yellow;
     constexpr auto colorMsg    = LogColor::White;
 
-    // Counts
     Logger::printHeaderDot(ctx, colorHeader, "numWorkers", colorMsg, Utf8Helper::toNiceBigNumber(ctx.global().jobMgr().numWorkers()));
+    Logger::printHeaderDot(ctx, colorHeader, "timeTotal", colorMsg, Utf8Helper::toNiceTime(Timer::toSeconds(timeTotal.load())));
+
 #if SWC_HAS_STATS
+    // Counts
+    Logger::print(ctx, "\n");
     Logger::printHeaderDot(ctx, colorHeader, "numFiles", colorMsg, Utf8Helper::toNiceBigNumber(numFiles.load()));
     Logger::printHeaderDot(ctx, colorHeader, "numTokens", colorMsg, Utf8Helper::toNiceBigNumber(numTokens.load()));
     Logger::printHeaderDot(ctx, colorHeader, "numAstNodes", colorMsg, Utf8Helper::toNiceBigNumber(numAstNodes.load()));
     Logger::printHeaderDot(ctx, colorHeader, "numVisitedAstNodes", colorMsg, Utf8Helper::toNiceBigNumber(numVisitedAstNodes.load()));
-#endif
+    
     Logger::print(ctx, "\n");
-
-    // Times
-#if SWC_HAS_STATS
     Logger::printHeaderDot(ctx, colorHeader, "timeLoadFile", colorMsg, Utf8Helper::toNiceTime(Timer::toSeconds(timeLoadFile.load())));
     Logger::printHeaderDot(ctx, colorHeader, "timeLexer", colorMsg, Utf8Helper::toNiceTime(Timer::toSeconds(timeLexer.load())));
     Logger::printHeaderDot(ctx, colorHeader, "timeParser", colorMsg, Utf8Helper::toNiceTime(Timer::toSeconds(timeParser.load())));
-#endif
-    Logger::printHeaderDot(ctx, colorHeader, "timeTotal", colorMsg, Utf8Helper::toNiceTime(Timer::toSeconds(timeTotal.load())));
-    Logger::print(ctx, "\n");
 
     // Memory
-#if SWC_HAS_STATS
+    Logger::print(ctx, "\n");
     Logger::printHeaderDot(ctx, colorHeader, "memMaxAllocated", colorMsg, Utf8Helper::toNiceSize(memMaxAllocated.load()));
 #endif
-    Logger::print(ctx, "\n");
 
+    Logger::print(ctx, "\n");
     log.unlock();
 }
 

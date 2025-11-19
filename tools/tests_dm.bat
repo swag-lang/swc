c41863435e8c9983f4643bf23c434ce6bb61c6c3
cb <christophe.beaudet@gmail.com>
2025-11-19 07:52:06 +0100

    Enable syntax verification for lexer in `tests_dm.bat`.

--- tools/tests_dm.bat
+++ tools/tests_dm.bat
@@ -1,4 +1,4 @@
 @echo off
-swc_devmode syntax --no-verify -d ../bin/tests/lexer %1 %2 %3 %4 %5 %6 %7 %8 %9
+swc_devmode syntax --verify -d ../bin/tests/lexer %1 %2 %3 %4 %5 %6 %7 %8 %9
 swc_devmode syntax --verify -d ../bin/tests/parser %1 %2 %3 %4 %5 %6 %7 %8 %9
 swc_devmode build -m ../bin/tests/legit %1 %2 %3 %4 %5 %6 %7 %8 %9

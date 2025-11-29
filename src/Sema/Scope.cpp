34709de3482048f23312e91e46be33b7c3623149
cb <christophe.beaudet@gmail.com>
2025-11-29 17:21:31 +0100

    Add `Scope` class definition and integrate it into the project

---
+++ src/Sema/Scope.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+#include "Sema/Scope.h"
+
+SWC_BEGIN_NAMESPACE()
+SWC_END_NAMESPACE()
---
+++ src/Sema/Scope.h
@@ -0,0 +1,8 @@
+#pragma once
+SWC_BEGIN_NAMESPACE()
+
+class Scope
+{
+};
+
+SWC_END_NAMESPACE()

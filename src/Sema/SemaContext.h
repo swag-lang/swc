179be6c6309db50d177b4dceaf514711495a107b
cb <christophe.beaudet@gmail.com>
2025-11-30 17:07:57 +0100

    Introduce sharded storage in `SemaContext` for concurrent symbol management.

--- src/Sema/SemaContext.h
+++ src/Sema/SemaContext.h
@@ -7,6 +7,15 @@ class SemaContext
 {
     Ast ast_;
 
+    struct Shard
+    {
+        std::shared_mutex                      mutex;
+        std::unordered_map<uint32_t, uint32_t> map;
+    };
+
+    constexpr static uint32_t NUM_SHARDS = 16;
+    Shard                     shards_[NUM_SHARDS];
+
 public:
     Ast&       ast() { return ast_; }
     const Ast& ast() const { return ast_; }

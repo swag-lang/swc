1c22b66e9c8bbddc0e93e5a9305207b00abae6eb
cb <christophe.beaudet@gmail.com>
2025-12-12 09:25:39 +0100

    Bump `SWC_REVISION` and `SWC_BUILD_NUM`; add semantic tests for `#swcrevision` and `#swcbuildnum` literals.

--- src/Main/Version.h
+++ src/Main/Version.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// Compiler version
 inline constexpr auto SWC_VERSION   = 0;
-inline constexpr auto SWC_REVISION  = 0;
-inline constexpr auto SWC_BUILD_NUM = 0;
-
-// Swag version
-inline constexpr auto SWAG_VERSION = SWC_VERSION;
+inline constexpr auto SWC_REVISION  = 1;
+inline constexpr auto SWC_BUILD_NUM = 1;

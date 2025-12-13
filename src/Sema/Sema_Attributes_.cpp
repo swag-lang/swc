e5eb568d8af6ab7125ef28b4f9b79cd96e9cae17
cb <christophe.beaudet@gmail.com>
2025-12-13 07:02:47 +0100

    Refactor `Sema`: replace `semaPostNode` with `semaPreNode` in `AstAccessModifier`, implement token-based access parsing, and update `AstNodesDef.inc`.

--- src/Sema/Sema_Attributes_.cpp
+++ src/Sema/Sema_Attributes_.cpp
@@ -3,8 +3,26 @@
 
 SWC_BEGIN_NAMESPACE()
 
-AstVisitStepResult AstAccessModifier::semaPostNode(Sema& sema) const
+AstVisitStepResult AstAccessModifier::semaPreNode(Sema& sema) const
 {
+    const Token& tok = sema.token(srcViewRef(), tokRef());
+
+    SymbolAccess access;
+    switch (tok.id)
+    {
+        case TokenId::KwdPrivate:
+            access = SymbolAccess::Private;
+            break;
+        case TokenId::KwdInternal:
+            access = SymbolAccess::Internal;
+            break;
+        case TokenId::KwdPublic:
+            access = SymbolAccess::Public;
+            break;
+        default:
+            SWC_UNREACHABLE();
+    }
+
     return AstVisitStepResult::Continue;
 }
 

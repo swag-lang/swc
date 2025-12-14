f03618ccd1961aacffd5f0dffdf3caa13cff0fcb
cb <christophe.beaudet@gmail.com>
2025-12-14 06:34:16 +0100

    Refactor `AstSuffixLiteral::semaPostNode` to `SemaCast.cpp`; expand character literal and type casting tests with additional edge and overflow case validations.

--- src/Sema/Sema_Type_.cpp
+++ src/Sema/Sema_Type_.cpp
@@ -2,7 +2,6 @@
 #include "Parser/AstVisit.h"
 #include "Sema/Constant/ConstantManager.h"
 #include "Sema/Sema.h"
-#include "Sema/SemaCast.h"
 #include "Sema/SemaInfo.h"
 #include "Sema/Type/TypeManager.h"
 
@@ -75,59 +74,6 @@ AstVisitStepResult AstBuiltinType::semaPostNode(Sema& sema) const
 
     sema.raiseInternalError(*this);
     return AstVisitStepResult::Stop;
-}
-
-AstVisitStepResult AstSuffixLiteral::semaPostNode(Sema& sema) const
-{
-    const auto&   ctx     = sema.ctx();
-    const TypeRef typeRef = sema.typeRefOf(nodeSuffixRef);
-
-    SWC_ASSERT(sema.hasConstant(nodeLiteralRef));
-
-    CastContext castCtx(CastKind::LiteralSuffix);
-    castCtx.errorNodeRef = nodeLiteralRef;
-
-    ConstantRef cstRef = sema.constantRefOf(nodeLiteralRef);
-
-    // Special case for negation: we need to negate before casting, in order for -128's8 to compile, for example.
-    // @MinusLiteralSuffix
-    if (const auto parentNode = sema.visit().parentNode(); parentNode->is(AstNodeId::UnaryExpr))
-    {
-        const Token& tok = sema.token(parentNode->srcViewRef(), parentNode->tokRef());
-        if (tok.is(TokenId::SymMinus))
-        {
-            const ConstantValue& cst  = sema.cstMgr().get(cstRef);
-            const TypeInfo&      type = sema.typeMgr().get(cst.typeRef());
-            if (type.isInt())
-            {
-                ApsInt cpy = cst.getInt();
-
-                bool overflow = false;
-                cpy.negate(overflow);
-                if (overflow)
-                {
-                    sema.raiseLiteralOverflow(nodeLiteralRef, cst, cst.typeRef());
-                    return AstVisitStepResult::Stop;
-                }
-
-                cpy.setUnsigned(false);
-                cstRef = sema.cstMgr().addConstant(ctx, ConstantValue::makeInt(ctx, cpy, cst.type(ctx).intBits()));
-            }
-            else if (type.isFloat())
-            {
-                ApFloat cpy = cst.getFloat();
-                cpy.negate();
-                cstRef = sema.cstMgr().addConstant(ctx, ConstantValue::makeFloat(ctx, cpy, cst.type(ctx).floatBits()));
-            }
-        }
-    }
-
-    const ConstantRef newCstRef = SemaCast::castConstant(sema, castCtx, cstRef, typeRef);
-    if (newCstRef.isInvalid())
-        return AstVisitStepResult::Stop;
-    sema.setConstant(sema.curNodeRef(), newCstRef);
-
-    return AstVisitStepResult::Continue;
 }
 
 AstVisitStepResult AstValueType::semaPostNode(Sema& sema) const

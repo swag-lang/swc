a8507cd56ed0fd4ac00006a816bbf666f9d82b68
cb <christophe.beaudet@gmail.com>
2026-01-20 06:31:21 +0100

    Refactor `TypeGen`: add `notifyAlive` call in `Sema` to ensure compiler state updates during type generation.

--- src/Sema/Type/TypeGen.cpp
+++ src/Sema/Type/TypeGen.cpp
@@ -470,6 +470,8 @@ namespace
 
         const TypeInfo& structType = tm.get(result.rtTypeRef);
         result.view                = std::string_view{storage.ptr<char>(result.offset), structType.sizeOf(ctx)};
+        
+        sema.compiler().notifyAlive();
         return Result::Continue;
     }
 }

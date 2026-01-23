b8e9116966d1a13e812420a5e8372109f6b54032
cb <christophe.beaudet@gmail.com>
2026-01-23 13:11:03 +0100

    Refactor `Sema`: add `semaPreNodeChild` and `semaPostNodeChild` methods for `AstIfStmt`, update `AstElseStmt` and `AstElseIfStmt` to use `pushScopeAutoPopOnPostNode`, and add corresponding tests.

---
+++ src/Sema/Ast/Sema.If.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "Sema/Core/Sema.h"
+#include "Parser/AstNodes.h"
+#include "Sema/Cast/Cast.h"
+#include "Sema/Core/SemaNodeView.h"
+#include "Sema/Helpers/SemaCheck.h"
+#include "Sema/Type/TypeManager.h"
+
+SWC_BEGIN_NAMESPACE();
+
+Result AstIfStmt::semaPreNodeChild(Sema& sema, const AstNodeRef& childRef) const
+{
+    if (childRef == nodeIfBlockRef || childRef == nodeElseBlockRef)
+        sema.pushScopeAutoPopOnPostChild(SemaScopeFlagsE::Local, childRef);
+
+    return Result::Continue;
+}
+
+Result AstIfStmt::semaPostNodeChild(Sema& sema, const AstNodeRef& childRef) const
+{
+    if (childRef == nodeConditionRef)
+    {
+        SemaNodeView nodeView(sema, nodeConditionRef);
+        RESULT_VERIFY(Cast::cast(sema, nodeView, sema.ctx().typeMgr().typeBool(), CastKind::Condition));
+    }
+
+    return Result::Continue;
+}
+
+Result AstElseStmt::semaPreNode(Sema& sema)
+{
+    sema.pushScopeAutoPopOnPostNode(SemaScopeFlagsE::Local);
+    return Result::Continue;
+}
+
+Result AstElseIfStmt::semaPreNode(Sema& sema)
+{
+    sema.pushScopeAutoPopOnPostNode(SemaScopeFlagsE::Local);
+    return Result::Continue;
+}
+
+SWC_END_NAMESPACE();

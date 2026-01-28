b46e205a65ec38205c0c9257926450dbb24f37e1
cb <christophe.beaudet@gmail.com>
2026-01-28 06:49:41 +0100

    Introduce `AstInfiniteLoopStmt` semantic analysis: add `semaPreNodeChild` to handle local scope and breakable loop frame setup.

--- src/Sema/Ast/Sema.While.cpp
+++ src/Sema/Ast/Sema.While.cpp
@@ -21,6 +21,20 @@ Result AstWhileStmt::semaPreNodeChild(Sema& sema, const AstNodeRef& childRef) co
     return Result::Continue;
 }
 
+Result AstInfiniteLoopStmt::semaPreNodeChild(Sema& sema, const AstNodeRef& childRef) const
+{
+    // Body has its own local scope.
+    if (childRef == nodeBodyRef)
+    {
+        SemaFrame frame = sema.frame();
+        frame.setBreakable(sema.curNodeRef(), SemaFrame::BreakableKind::Loop);
+        sema.pushFrameAutoPopOnPostChild(frame, childRef);
+        sema.pushScopeAutoPopOnPostChild(SemaScopeFlagsE::Local, childRef);
+    }
+
+    return Result::Continue;
+}
+
 Result AstWhileStmt::semaPostNodeChild(Sema& sema, const AstNodeRef& childRef) const
 {
     // Ensure while condition is `bool` (or castable to it).
@@ -35,8 +49,8 @@ Result AstWhileStmt::semaPostNodeChild(Sema& sema, const AstNodeRef& childRef) c
 
 Result AstBreakStmt::semaPreNode(Sema& sema)
 {
-    //if (sema.frame().breakableKind() == SemaFrame::BreakableKind::None)
-    //    return SemaError::raise(sema, DiagnosticId::sema_err_break_outside_breakable, sema.curNodeRef());
+    if (sema.frame().breakableKind() == SemaFrame::BreakableKind::None)
+        return SemaError::raise(sema, DiagnosticId::sema_err_break_outside_breakable, sema.curNodeRef());
     return Result::Continue;
 }
 

b7b1d14d160f4846c25078574c69ad24cf4a46c5
cb <christophe.beaudet@gmail.com>
2026-02-05 06:34:19 +0100

    Refactor: Implement `semaPreNode`, `semaPreNodeChild`, and `semaPostNodeChild` for `AstForStmt

--- src/Compiler/Sema/Ast/Sema.Loop.cpp
+++ src/Compiler/Sema/Ast/Sema.Loop.cpp
@@ -3,9 +3,140 @@
 #include "Compiler/Parser/Ast/AstNodes.h"
 #include "Compiler/Sema/Cast/Cast.h"
 #include "Compiler/Sema/Core/SemaNodeView.h"
+#include "Compiler/Sema/Helpers/SemaCheck.h"
 #include "Compiler/Sema/Helpers/SemaError.h"
+#include "Compiler/Sema/Helpers/SemaHelpers.h"
+#include "Compiler/Sema/Match/Match.h"
 
 SWC_BEGIN_NAMESPACE();
+
+namespace
+{
+    Result checkForCountExpr(Sema& sema, AstNodeRef nodeRef)
+    {
+        const SemaNodeView nodeView(sema, nodeRef);
+
+        if (nodeView.type && nodeView.type->isInt())
+            return Result::Continue;
+
+        bool countOfOk = false;
+        if (nodeView.type)
+        {
+            if (nodeView.cst && (nodeView.cst->isString() || nodeView.cst->isSlice()))
+                countOfOk = true;
+            else if (nodeView.type->isEnum())
+            {
+                RESULT_VERIFY(sema.waitCompleted(nodeView.type, nodeView.nodeRef));
+                countOfOk = true;
+            }
+            else if (nodeView.type->isAnyString() || nodeView.type->isArray() || nodeView.type->isSlice())
+            {
+                countOfOk = true;
+            }
+        }
+
+        if (countOfOk)
+            return Result::Continue;
+
+        if (!nodeView.type)
+            return SemaError::raise(sema, DiagnosticId::sema_err_invalid_countof, nodeView.nodeRef);
+
+        auto diag = SemaError::report(sema, DiagnosticId::sema_err_invalid_countof_type, nodeView.nodeRef);
+        diag.addArgument(Diagnostic::ARG_TYPE, nodeView.typeRef);
+        diag.report(sema.ctx());
+        return Result::Error;
+    }
+
+    TypeRef getForIndexType(Sema& sema, AstNodeRef nodeRef)
+    {
+        const SemaNodeView nodeView(sema, nodeRef);
+
+        if (nodeView.type && nodeView.type->isInt())
+            return nodeView.typeRef;
+
+        return sema.typeMgr().typeU64();
+    }
+}
+
+Result AstForStmt::semaPreNode(Sema& sema) const
+{
+    return SemaCheck::modifiers(sema, *this, modifierFlags, AstModifierFlagsE::Reverse);
+}
+
+Result AstForStmt::semaPreNodeChild(Sema& sema, const AstNodeRef& childRef) const
+{
+    // Body has its own local scope.
+    if (childRef == nodeBodyRef)
+    {
+        // TODO
+        if (sema.file()->isRuntime())
+            return Result::SkipChildren;
+        
+        SemaFrame frame = sema.frame();
+        frame.setCurrentBreakContent(sema.curNodeRef(), SemaFrame::BreakContextKind::Loop);
+        sema.pushFramePopOnPostChild(frame, childRef);
+        sema.pushScopePopOnPostChild(SemaScopeFlagsE::Local, childRef);
+
+        if (tokNameRef.isValid())
+        {
+            const SemaNodeView nodeView(sema, nodeExprRef);
+            TypeRef            indexTypeRef = TypeRef::invalid();
+            if (const auto* range = nodeView.node->safeCast<AstRangeExpr>())
+            {
+                if (range->nodeExprDownRef.isValid())
+                    indexTypeRef = getForIndexType(sema, range->nodeExprDownRef);
+                if (indexTypeRef.isInvalid() && range->nodeExprUpRef.isValid())
+                    indexTypeRef = getForIndexType(sema, range->nodeExprUpRef);
+            }
+            else
+            {
+                indexTypeRef = getForIndexType(sema, nodeExprRef);
+            }
+
+            if (indexTypeRef.isInvalid())
+                return SemaError::raiseInternal(sema, sema.curNodeRef());
+
+            auto& symVar = SemaHelpers::registerSymbol<SymbolVariable>(sema, *this, tokNameRef);
+            symVar.registerAttributes(sema);
+            symVar.setDeclared(sema.ctx());
+            RESULT_VERIFY(Match::ghosting(sema, symVar));
+
+            symVar.addExtraFlag(SymbolVariableFlagsE::Initialized);
+            symVar.setTypeRef(indexTypeRef);
+            symVar.setTyped(sema.ctx());
+            symVar.setCompleted(sema.ctx());
+        }
+    }
+
+    return Result::Continue;
+}
+
+Result AstForStmt::semaPostNodeChild(Sema& sema, const AstNodeRef& childRef) const
+{
+    if (childRef == nodeExprRef)
+    {
+        const SemaNodeView nodeView(sema, nodeExprRef);
+        if (const auto* range = nodeView.node->safeCast<AstRangeExpr>())
+        {
+            if (range->nodeExprDownRef.isValid())
+                RESULT_VERIFY(checkForCountExpr(sema, range->nodeExprDownRef));
+            if (range->nodeExprUpRef.isValid())
+                RESULT_VERIFY(checkForCountExpr(sema, range->nodeExprUpRef));
+        }
+        else
+        {
+            RESULT_VERIFY(checkForCountExpr(sema, nodeExprRef));
+        }
+    }
+
+    if (childRef == nodeWhereRef)
+    {
+        SemaNodeView nodeView(sema, nodeWhereRef);
+        RESULT_VERIFY(Cast::cast(sema, nodeView, sema.typeMgr().typeBool(), CastKind::Condition));
+    }
+
+    return Result::Continue;
+}
 
 Result AstWhileStmt::semaPreNodeChild(Sema& sema, const AstNodeRef& childRef) const
 {

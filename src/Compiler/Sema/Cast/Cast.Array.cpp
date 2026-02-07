778ffcd6bfe5835e057e7e5e02f44cbfc89903cb
cb <christophe.beaudet@gmail.com>
2026-02-07 10:27:44 +0100

    Refactor: add dedicated diagnostic functions for array casting errors and update related tests in `Sema.Cast`

--- src/Compiler/Sema/Cast/Cast.Array.cpp
+++ src/Compiler/Sema/Cast/Cast.Array.cpp
@@ -19,6 +19,44 @@ namespace
         const TypeInfo* dstType;
     };
 
+    Result failArrayDimCount(const CastArrayContext& ctx, size_t srcCount, size_t dstCount)
+    {
+        const Result res = ctx.castCtx->fail(DiagnosticId::sema_err_array_cast_num_dims, ctx.srcTypeRef, ctx.dstTypeRef);
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_COUNT, static_cast<uint64_t>(srcCount));
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_VALUE, static_cast<uint64_t>(dstCount));
+        return res;
+    }
+
+    Result failArrayDimMismatch(const CastArrayContext& ctx, size_t dimIndex, uint64_t srcDim, uint64_t dstDim)
+    {
+        Utf8 detail = "dim ";
+        detail += std::to_string(dimIndex + 1);
+        detail += ": src = '";
+        detail += std::to_string(srcDim);
+        detail += "' dst = '";
+        detail += std::to_string(dstDim);
+        detail += "'";
+
+        const Result res = ctx.castCtx->fail(DiagnosticId::sema_err_array_cast_dim_mismatch, ctx.srcTypeRef, ctx.dstTypeRef);
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_VALUE, detail);
+        return res;
+    }
+
+    Result failArrayTooManyValues(const CastArrayContext& ctx, size_t srcCount, uint64_t dstCount)
+    {
+        const Result res = ctx.castCtx->fail(DiagnosticId::sema_err_array_cast_too_many_values, ctx.srcTypeRef, ctx.dstTypeRef);
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_COUNT, static_cast<uint64_t>(srcCount));
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_VALUE, dstCount);
+        return res;
+    }
+
+    Result failArrayConst(const CastArrayContext& ctx, std::string_view reason)
+    {
+        const Result res = ctx.castCtx->fail(DiagnosticId::sema_err_array_cast_const, ctx.srcTypeRef, ctx.dstTypeRef);
+        ctx.castCtx->failure.addArgument(Diagnostic::ARG_VALUE, reason);
+        return res;
+    }
+
     Result checkElemCast(const CastArrayContext& ctx, TypeRef srcElemType, TypeRef dstElemType)
     {
         CastContext elemCtx(ctx.castCtx->kind);
@@ -56,12 +94,12 @@ namespace
 
         const auto& srcDims = ctx.srcType->payloadArrayDims();
         if (srcDims.size() != dstDims.size())
-            return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+            return failArrayDimCount(ctx, srcDims.size(), dstDims.size());
 
         for (size_t i = 0; i < srcDims.size(); ++i)
         {
             if (srcDims[i] != dstDims[i])
-                return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+                return failArrayDimMismatch(ctx, i, srcDims[i], dstDims[i]);
         }
 
         const TypeRef srcElemTypeRef = ctx.srcType->payloadArrayElemTypeRef();
@@ -69,11 +107,11 @@ namespace
             return Result::Continue;
 
         if (!ctx.castCtx->isConstantFolding())
-            return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+            return failArrayConst(ctx, "array element cast requires constant folding");
 
         const ConstantValue& cst = ctx.sema->cstMgr().get(ctx.castCtx->constantFoldingSrc());
         if (!cst.isAggregateArray())
-            return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+            return failArrayConst(ctx, "expected an array constant");
 
         const auto&              values = cst.getAggregateArray();
         std::vector<ConstantRef> newValues;
@@ -104,7 +142,7 @@ namespace
             totalCount *= dim;
 
         if (srcTypes.size() > totalCount)
-            return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+            return failArrayTooManyValues(ctx, srcTypes.size(), totalCount);
 
         for (const auto srcElemTypeRef : srcTypes)
         {
@@ -118,7 +156,7 @@ namespace
 
         const ConstantValue& cst = ctx.sema->cstMgr().get(ctx.castCtx->constantFoldingSrc());
         if (!cst.isAggregateArray())
-            return ctx.castCtx->fail(DiagnosticId::sema_err_cannot_cast, ctx.srcTypeRef, ctx.dstTypeRef);
+            return failArrayConst(ctx, "expected an array constant");
 
         const auto&              values = cst.getAggregateArray();
         std::vector<ConstantRef> newValues;

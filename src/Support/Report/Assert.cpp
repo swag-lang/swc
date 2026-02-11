e966631e98b8693cd3e731213d19499ad497651c
cb <christophe.beaudet@gmail.com>
2026-02-11 19:53:10 +0100

    Update error reporting format in `swcAssert` and `swcInternalError` to use `File(line)` style for improved readability.

--- src/Support/Report/Assert.cpp
+++ src/Support/Report/Assert.cpp
@@ -6,13 +6,13 @@ SWC_BEGIN_NAMESPACE();
 
 void swcAssert(const char* expr, const char* file, int line)
 {
-    const Utf8 msg = std::format("assertion failed!\n\nFile: {}\nLine: {}\nExpression: {}\n", file, line, expr);
+    const Utf8 msg = std::format("assertion failed!\n\nFile: {}({})\nExpression: {}\n", file, line, expr);
     Os::panicBox(msg.c_str());
 }
 
 [[noreturn]] void swcInternalError(const TaskContext& ctx, const char* file, int line)
 {
-    const Utf8 msg = std::format("internal error!\n\nFile: {}\nLine: {}\n", file ? file : "<null>", line);
+    const Utf8 msg = std::format("internal error!\n\nFile: {}({})\n", file ? file : "<null>", line);
     Os::panicBox(msg.c_str());
 }
 

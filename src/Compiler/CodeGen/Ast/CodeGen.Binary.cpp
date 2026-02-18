cd077e12d7a863f52b068aa51d555691c52e9a79
cb <christophe.beaudet@gmail.com>
2026-02-18 16:13:35 +0100

    Refactor: Split `codeGenBinaryPlus` into specialized functions for integer and floating-point addition, and centralize operand kind handling for improved clarity and maintainability in `CodeGen.Binary.cpp`

--- src/Compiler/CodeGen/Ast/CodeGen.Binary.cpp
+++ src/Compiler/CodeGen/Ast/CodeGen.Binary.cpp
@@ -9,64 +9,108 @@ SWC_BEGIN_NAMESPACE();
 
 namespace
 {
-    MicroOpBits plusOpBits(const TypeInfo& resultType)
+    enum class BinaryOperandKind : uint8_t
     {
-        if (resultType.isFloat())
-            return microOpBitsFromBitWidth(resultType.payloadFloatBits());
+        IntLike,
+        Float,
+        Other,
+    };
 
-        if (resultType.isInt())
+    BinaryOperandKind operandKind(const TypeInfo& type)
+    {
+        if (type.isIntLike())
+            return BinaryOperandKind::IntLike;
+
+        if (type.isFloat())
+            return BinaryOperandKind::Float;
+
+        return BinaryOperandKind::Other;
+    }
+
+    MicroOpBits arithmeticOpBits(const TypeInfo& type, BinaryOperandKind kind)
+    {
+        if (kind == BinaryOperandKind::Float)
         {
-            const uint32_t intBits = resultType.payloadIntBits() ? resultType.payloadIntBits() : 64;
+            const uint32_t floatBits = type.payloadFloatBits() ? type.payloadFloatBits() : 64;
+            return microOpBitsFromBitWidth(floatBits);
+        }
+
+        if (kind == BinaryOperandKind::IntLike)
+        {
+            const uint32_t intBits = type.payloadIntLikeBits() ? type.payloadIntLikeBits() : 64;
             return microOpBitsFromBitWidth(intBits);
         }
 
         return MicroOpBits::Zero;
     }
 
-    void materializeBinaryOperand(MicroReg& outReg, CodeGen& codeGen, const CodeGenNodePayload& operandPayload, TypeRef resultTypeRef, MicroOpBits opBits)
+    void materializeBinaryOperand(MicroReg& outReg, CodeGen& codeGen, const CodeGenNodePayload& operandPayload, TypeRef operandTypeRef, MicroOpBits opBits)
     {
         MicroBuilder& builder = codeGen.builder();
-        outReg                = codeGen.nextVirtualRegisterForType(resultTypeRef);
-
+        outReg                = codeGen.nextVirtualRegisterForType(operandTypeRef);
         if (operandPayload.storageKind == CodeGenNodePayload::StorageKind::Address)
-        {
             builder.encodeLoadRegMem(outReg, operandPayload.reg, 0, opBits);
-            return;
-        }
-
-        builder.encodeLoadRegReg(outReg, operandPayload.reg, opBits);
+        else
+            builder.encodeLoadRegReg(outReg, operandPayload.reg, opBits);
     }
 
-    Result codeGenBinaryPlus(CodeGen& codeGen, const AstBinaryExpr& node)
+    Result emitPlusIntLikeIntLike(CodeGen& codeGen, const AstBinaryExpr& node, const SemaNodeView& leftView, const SemaNodeView& rightView)
     {
-        const SemaNodeView nodeView = codeGen.curNodeView();
-        SWC_ASSERT(nodeView.type);
-        const TypeInfo& resultType = *nodeView.type;
-
-        if (!resultType.isInt() && !resultType.isFloat())
-            return Result::Continue;
-
+        SWC_ASSERT(leftView.type && rightView.type);
         const CodeGenNodePayload* leftPayload  = codeGen.payload(node.nodeLeftRef);
         const CodeGenNodePayload* rightPayload = codeGen.payload(node.nodeRightRef);
         SWC_ASSERT(leftPayload != nullptr);
         SWC_ASSERT(rightPayload != nullptr);
 
-        const MicroOpBits opBits = plusOpBits(resultType);
+        const MicroOpBits opBits = arithmeticOpBits(*leftView.type, BinaryOperandKind::IntLike);
         SWC_ASSERT(opBits != MicroOpBits::Zero);
 
-        CodeGenNodePayload& nodePayload = codeGen.setPayload(codeGen.curNodeRef(), nodeView.typeRef);
-        nodePayload.reg                 = codeGen.nextVirtualRegisterForType(nodeView.typeRef);
+        CodeGenNodePayload& nodePayload = codeGen.setPayload(codeGen.curNodeRef(), codeGen.curNodeView().typeRef);
         nodePayload.storageKind         = CodeGenNodePayload::StorageKind::Value;
 
         MicroReg rightReg = MicroReg::invalid();
-        materializeBinaryOperand(nodePayload.reg, codeGen, *leftPayload, nodeView.typeRef, opBits);
-        materializeBinaryOperand(rightReg, codeGen, *rightPayload, nodeView.typeRef, opBits);
+        materializeBinaryOperand(nodePayload.reg, codeGen, *leftPayload, leftView.typeRef, opBits);
+        materializeBinaryOperand(rightReg, codeGen, *rightPayload, rightView.typeRef, opBits);
 
-        MicroBuilder& builder = codeGen.builder();
-        if (resultType.isFloat())
-            builder.encodeOpBinaryRegReg(nodePayload.reg, rightReg, MicroOp::FloatAdd, opBits);
-        else
-            builder.encodeOpBinaryRegReg(nodePayload.reg, rightReg, MicroOp::Add, opBits);
+        codeGen.builder().encodeOpBinaryRegReg(nodePayload.reg, rightReg, MicroOp::Add, opBits);
+        return Result::Continue;
+    }
+
+    Result emitPlusFloatFloat(CodeGen& codeGen, const AstBinaryExpr& node, const SemaNodeView& leftView, const SemaNodeView& rightView)
+    {
+        SWC_ASSERT(leftView.type && rightView.type);
+        const CodeGenNodePayload* leftPayload  = codeGen.payload(node.nodeLeftRef);
+        const CodeGenNodePayload* rightPayload = codeGen.payload(node.nodeRightRef);
+        SWC_ASSERT(leftPayload != nullptr);
+        SWC_ASSERT(rightPayload != nullptr);
+
+        const MicroOpBits opBits = arithmeticOpBits(*leftView.type, BinaryOperandKind::Float);
+        SWC_ASSERT(opBits != MicroOpBits::Zero);
+
+        CodeGenNodePayload& nodePayload = codeGen.setPayload(codeGen.curNodeRef(), codeGen.curNodeView().typeRef);
+        nodePayload.storageKind         = CodeGenNodePayload::StorageKind::Value;
+
+        MicroReg rightReg = MicroReg::invalid();
+        materializeBinaryOperand(nodePayload.reg, codeGen, *leftPayload, leftView.typeRef, opBits);
+        materializeBinaryOperand(rightReg, codeGen, *rightPayload, rightView.typeRef, opBits);
+
+        codeGen.builder().encodeOpBinaryRegReg(nodePayload.reg, rightReg, MicroOp::FloatAdd, opBits);
+        return Result::Continue;
+    }
+
+    Result codeGenBinaryPlus(CodeGen& codeGen, const AstBinaryExpr& node)
+    {
+        const SemaNodeView leftView  = codeGen.nodeView(node.nodeLeftRef);
+        const SemaNodeView rightView = codeGen.nodeView(node.nodeRightRef);
+        SWC_ASSERT(leftView.type && rightView.type);
+
+        const BinaryOperandKind leftKind  = operandKind(*leftView.type);
+        const BinaryOperandKind rightKind = operandKind(*rightView.type);
+
+        if (leftKind == BinaryOperandKind::IntLike && rightKind == BinaryOperandKind::IntLike)
+            return emitPlusIntLikeIntLike(codeGen, node, leftView, rightView);
+        if (leftKind == BinaryOperandKind::Float && rightKind == BinaryOperandKind::Float)
+            return emitPlusFloatFloat(codeGen, node, leftView, rightView);
 
         return Result::Continue;
     }

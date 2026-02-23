fa51f1fbba3bef3537d9c4e88b8e269e7e9c107f
cb <christophe.beaudet@gmail.com>
2026-02-23 13:12:16 +0100

    Add O3 optimization level to batch scripts (`all_dm.bat` and `all.bat`)

--- tools/all.bat
+++ tools/all.bat
@@ -3,7 +3,7 @@
 call "%~dp0_suite.bat" syntax swc %*
 if errorlevel 1 exit /b %errorlevel%
 
-for %%O in (O0 O1 O2 Os Oz) do (
+for %%O in (O0 O1 O2 O3 Os Oz) do (
     call "%~dp0_suite.bat" sema swc --backend-optimize %%O %*
     if errorlevel 1 exit /b %errorlevel%
 )

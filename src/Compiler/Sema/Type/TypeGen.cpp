d22cd52fffac86cd2eaf3648d4268da9a1121248
cb <christophe.beaudet@gmail.com>
2026-03-07 08:22:19 +0100

    Refactor `TypeGen` cache handling and type generation logic
    - Replaced manual cache initialization with `std::unordered_map::try_emplace` for cleaner and more efficient cache updates in `cacheFor`.
    - Simplified `makeTypeInfo` logic by leveraging `SWC_RESULT_VERIFY` and ensuring back reference storage is synchronized after processing.

--- src/Compiler/Sema/Type/TypeGen.cpp
+++ src/Compiler/Sema/Type/TypeGen.cpp
@@ -7,11 +7,13 @@ SWC_BEGIN_NAMESPACE();
 
 TypeGen::TypeGenCache& TypeGen::cacheFor(const DataSegment& storage)
 {
-    const std::scoped_lock lk(cachesMutex_);
-    auto&                  ptr = caches_[&storage];
-    if (!ptr)
-        ptr = std::make_unique<TypeGenCache>();
-    return *ptr;
+    std::scoped_lock lk(cachesMutex_);
+
+    auto [it, inserted] = caches_.try_emplace(&storage);
+    if (!it->second)
+        it->second = std::make_unique<TypeGenCache>();
+
+    return *it->second;
 }
 
 Result TypeGen::makeTypeInfo(Sema& sema, DataSegment& storage, TypeRef typeRef, AstNodeRef ownerNodeRef, TypeGenResult& result)
@@ -21,14 +23,13 @@ Result TypeGen::makeTypeInfo(Sema& sema, DataSegment& storage, TypeRef typeRef, 
 
     // Each call progresses as much as possible without relying on recursion.
     // It returns Result::Continue only when the requested type AND all its dependencies are fully done.
-    const auto res = TypeGenInternal::processTypeInfo(sema, result, storage, typeRef, ownerNodeRef, cache);
-    if (res == Result::Continue)
-    {
-        const std::scoped_lock lk2(ptrToTypeMutex_);
-        ptrToType_[result.span.data()] = typeRef;
-    }
+    SWC_RESULT_VERIFY(TypeGenInternal::processTypeInfo(sema, result, storage, typeRef, ownerNodeRef, cache));
 
-    return res;
+    // Now that we are done, store back reference
+    const std::scoped_lock lk2(ptrToTypeMutex_);
+    ptrToType_[result.span.data()] = typeRef;
+    
+    return Result::Continue;
 }
 
 TypeRef TypeGen::getBackTypeRef(const void* ptr) const

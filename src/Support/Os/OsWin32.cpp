4dff4aa787d1acfae5de13b53c23ab9c234ae9c7
cb <christophe.beaudet@gmail.com>
2026-03-08 18:11:08 +0100

    Remove leftover commented-out debug code in `OsWin32.cpp`.

--- src/Support/Os/OsWin32.cpp
+++ src/Support/Os/OsWin32.cpp
@@ -464,7 +464,6 @@ namespace Os
         memoryCounters.cb                      = sizeof(memoryCounters);
         if (!GetProcessMemoryInfo(GetCurrentProcess(), &memoryCounters, sizeof(memoryCounters)))
             return 0;
-        // std::this_thread::sleep_for(std::chrono::milliseconds(10000));
         return memoryCounters.PeakWorkingSetSize;
     }
 

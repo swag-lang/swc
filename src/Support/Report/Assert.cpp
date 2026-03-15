c872af26d23a5ee0faab6e40586c7b0ee79eb96c
cb <christophe.beaudet@gmail.com>
2026-03-15 07:37:16 +0100

    Standardize error messages in `Assert.cpp` by capitalizing initial words for consistency.

--- src/Support/Report/Assert.cpp
+++ src/Support/Report/Assert.cpp
@@ -7,14 +7,14 @@ SWC_BEGIN_NAMESPACE();
 void swcAssert(const char* expr, const char* file, int line)
 {
     const Utf8 fileLoc = FileSystem::formatFileLocation(nullptr, fs::path(file ? file : "<null>"), static_cast<uint32_t>(line));
-    const Utf8 msg     = std::format("assertion failed!\n\nFile: {}\nExpression: {}\n", fileLoc, expr);
+    const Utf8 msg     = std::format("Assertion Failed!\nFile: {}\nExpression: {}\n", fileLoc, expr);
     Os::panicBox(msg.c_str());
 }
 
 void swcInternalError(const char* file, int line, const char* expr)
 {
     const Utf8 fileLoc = FileSystem::formatFileLocation(nullptr, fs::path(file ? file : "<null>"), static_cast<uint32_t>(line));
-    Utf8       msg     = std::format("internal error!\n\nFile: {}\n", fileLoc);
+    Utf8       msg     = std::format("Internal Error!\nFile: {}\n", fileLoc);
     if (expr)
         msg += std::format("Expression: {}\n", expr);
     Os::panicBox(msg.c_str());

1bbe8f1a8fcd484acd2462b74b2eebfef68bb0e3
cb <christophe.beaudet@gmail.com>
2026-04-08 13:46:08 +0200

    Add batch scripts for reference test outputs and re-enable commented boolean literal test

---
+++ tools/reference_dm.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+for %%I in ("%~dp0..") do set "ROOT=%%~fI"
+set "NATIVE_OUTPUT=%ROOT%\.output"
+set "REFERENCE_OUTPUT=%NATIVE_OUTPUT%\reference\devmode"
+set "REFERENCE_WORKDIR=%NATIVE_OUTPUT%\work\reference\devmode"
+
+swc_devmode test -m "%ROOT%\bin\reference\tests\language" --artifact-kind exe --out-dir "%REFERENCE_OUTPUT%" --work-dir "%REFERENCE_WORKDIR%" %*
+if errorlevel 1 exit /b 1
+
+exit /b 0

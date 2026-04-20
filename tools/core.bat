da29d203f3e93f5d37cbaef28d4c876fff11380f
cb <christophe.beaudet@gmail.com>
2026-04-20 07:30:01 +0200

    Integrate Win32 and XInput API directories into `core.bat` and update module import paths.

--- tools/core.bat
+++ tools/core.bat
@@ -7,6 +7,8 @@ if errorlevel 1 exit /b %errorlevel%
 if /I "%~1"=="dm" shift
 
 set "CORE_MODULE=%ROOT%\bin\std\modules\core"
+set "WIN32_API_DIR=%OUTPUT_ROOT%\dep\win32"
+set "XINPUT_API_DIR=%OUTPUT_ROOT%\dep\xinput"
 set "EXTRA_ARGS="
 
 :parse_args
@@ -16,7 +18,7 @@ shift
 goto parse_args
 
 :run
-%SWC_EXE% sema -m "%CORE_MODULE%" --module-namespace Core%EXTRA_ARGS%
+%SWC_EXE% sema -m "%CORE_MODULE%" --module-namespace Core --import-api-dir "%WIN32_API_DIR%" --import-api-dir "%XINPUT_API_DIR%"%EXTRA_ARGS%
 if errorlevel 1 exit /b 1
 
 exit /b 0
